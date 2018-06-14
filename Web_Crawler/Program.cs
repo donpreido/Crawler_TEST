58e128309fa39119f26b3cf8913ff19477e8defe
donpreido <donatas.preidys@gmail.com>
2018-06-14 22:33:55 +0300

    Initial check-in of Crawler

---
+++ Web_Crawler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Web_Crawler
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
